25c85ff20899cc1d0198d7a48d1ebe694684ef5f
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2023-01-25 08:50:36 +0000

    Add the parameter "data" in the function to be less specific

--- Upload_data function.R
+++ Upload_data function.R
@@ -6,15 +6,16 @@
 #'
 #'
 #' @param input_path folder where the Redcap data are
+#' @param data RKD data used
 #' @param ouput_path folder where the Redcap data will be saved
 #' @param date date when you load your data
 #' @return The Redcap data in your folder and in an object
 #' @export
 
 
-LoadRKD=function(input_path, output_path,date){
+LoadRKD=function(input_path, data, output_path, date){
   setwd(input_path)
-  data <- read.csv("", header=T)
+  data <- read.csv(data, header=T)
   return(data)
   setwd(output_path)
   write.csv(paste("Redcap_clinical_data_",date,sep=""), data, row.names = F)
