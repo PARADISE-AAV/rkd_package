83823782327d3405ba786f7f6d884fa3f0263dd4
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2023-02-24 11:52:57 +0000

    small correction of name

--- LoadBiomarker.R
+++ LoadBiomarker.R
@@ -16,26 +16,20 @@ LoadRKD=function(input_path, file_name){
   if (is.character(input_path) == FALSE) {
     stop("The argument input_path need to be a character argument")
   }
-  if (is.character(files) == FALSE) {
+  if (is.character(file_name) == FALSE) {
     stop("The argument files need to be a character argument")
   }
-  if (is.character(output_path) == FALSE) {
-    stop("The argument output_path need to be a character argument")
-  }
-  if (is.character(date) == FALSE) {
-    stop("The argument date need to be a character argument")
-  }
-  
+ 
   files_test <-  list.files(input_path, pattern = ".", all.files = FALSE, recursive = TRUE)
   if(identical(files_test, character(0)) == TRUE){
     stop("Your input folder don't exist")
   }
-  if(length(which(files_test == files)) != 1){
+  if(length(which(files_test == file-name)) != 1){
     stop("There is no files requested or multiple times this files")
   }
   
   setwd(input_path)
-  Biomarker_data <- read.csv(files, header = TRUE)
+  Biomarker_data <- read.csv(file_name, header = TRUE)
   ###check that you load a real file
   if(ncol(Biomarker_data)==0 | nrow(Biomarker_data)==0){
     stop("You give an empty files")
