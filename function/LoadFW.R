862984911ba7272ec965c4dd601bb32f6f7864e9
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2023-03-21 14:29:31 +0000

    Create LoadFW.R

---
+++ function/LoadFW.R
@@ -0,0 +1,43 @@
+#' @title LoadFW
+#' @author Matthieu COQ
+#' Version: 1.0
+#' Date: 24-Jan-23
+#' Objective: The objective is to load the Freezerwork data in a datframe object
+#'
+#'
+#' @param folder_name folder name of the Freezerwork
+#' @param matrix_type type of blood matrix needed
+#' @return The Redcap data in your folder and in an object
+#' @export
+
+LoadFW=function(folder_name, matrix_type){
+  ####Test on the argument
+  if (is.character(folder_name) == FALSE) {
+    stop("The argument files_name need to be a character argument")
+  }
+  if (is.character(matrix_type) == FALSE) {
+    stop("The argument files_name need to be a character argument")
+  }
+  
+  #extract the folder and the files 
+  
+  
+  files_test <-  list.files(folder_name, pattern = ".", all.files = FALSE, recursive = TRUE)
+  if(identical(files_test, character(0)) == TRUE){
+    stop("Your input folder don't exist")
+  }
+  files_name1=files_test[grep(matrix_type,files_test)]
+  
+  if(length(which(files_test == files_name1)) != 1){
+    stop("There is no files requested or multiple times this files")
+  }
+  
+  
+  FW_data <- read.csv(files_name1, header = TRUE)
+  ###check that you load a real file
+  if(ncol(FW_data)==0 | nrow(RKD_data)==0){
+    stop("You give an empty files")
+  }
+  
+  return(FW_data)
+}
