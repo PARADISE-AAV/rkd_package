1dfbab2f946b02d2cad6eb829c0e89b8ff104d41
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2023-03-22 10:20:37 +0000

    Create FindFWSample function.R

---
+++ function/FindFWSample function.R
@@ -0,0 +1,11 @@
+#' @title FindFWSample
+#' @author Matthieu COQ
+#' Version: 1.0
+#' Date: 22-Mar-23
+#' Objective: The objective is to merge the RKD data filter and clean with the clean Freezer work data
+#'
+#'
+#' @param FWdata FW data from CleanFW function
+#' @param ouput_path folder where the merged data will be saved
+#' @return The Redcap data cleaned in your folder and in an object
+#' @export
