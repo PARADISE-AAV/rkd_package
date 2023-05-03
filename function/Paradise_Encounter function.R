c88a275c1e547977589cfc45a24259ddaa6f327f
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2023-05-03 09:38:30 +0100

    Create Paradise_Encounter function.R

---
+++ function/Paradise_Encounter function.R
@@ -0,0 +1,18 @@
+#' @title Paradise_Encounter
+#' @author Yagmur Dogay/Matthieu Coq
+#' Version: 1.0
+#' Date: 03-May-23
+#' Objective: Selection of the Encounter that meet the Paradise criteria
+#'
+
+
+#' @param RKD_data RKD data from Demographic Filter RKD Data
+#' @return The data with the a variable that tell you if the Encounter match the paradise criteria 
+#' @details The criteria to be a Paradise encounter are 1/ need to be in Remission, 2/ need to in remission >6 month 3/ Have more than 1 year follow up
+#' @export
+#' 
+
+Paradise_Encounter <- function(RKD_data){
+  
+}
+
