eedb7e8a49efca3bef5b5af76932f95ee65f447a
dogayy <123482827+dogayy@users.noreply.github.com>
2023-05-11 13:54:15 +0100

    Update LoadBiomarker.R
    updated the load biomarker function

--- function/LoadBiomarker.R
+++ function/LoadBiomarker.R
@@ -11,29 +11,36 @@
 
 
 
-LoadBiomarker=function(input_path, file_name){
+LoadBiomarker=function(files_name){
   ####Test on the argument
-  if (is.character(input_path) == FALSE) {
-    stop("The argument input_path need to be a character argument")
+  library(stringr)
+  if (is.character(files_name) == FALSE) {
+    stop("The argument files_name need to be a character argument")
   }
-  if (is.character(file_name) == FALSE) {
-    stop("The argument files need to be a character argument")
+  #extract the folder and the files 
+  b <- max(gregexpr("\\/", files_name)[[1]])
+  if(b > 0){
+    input_path <- str_sub(files_name, 1, b)
+    files_name1 <- str_sub(files_name, b+1, nchar(files_name))
+  }else{
+    input_path <- getwd()
+    files_name1 <- files_name
   }
- 
+  dir_test <- list.dirs(input_path)
   files_test <-  list.files(input_path, pattern = ".", all.files = FALSE, recursive = TRUE)
-  if(identical(files_test, character(0)) == TRUE){
+  if(identical(dir_test, character(0)) == TRUE){
     stop("Your input folder don't exist")
   }
-  if(length(which(files_test == file-name)) != 1){
+  if(length(which(files_test == files_name1)) != 1){
     stop("There is no files requested or multiple times this files")
   }
   
-  setwd(input_path)
-  Biomarker_data <- read.csv(file_name, header = TRUE)
+  
+  Biomarker_data <- read.csv(files_name, header = TRUE)
   ###check that you load a real file
-  if(ncol(Biomarker_data)==0 | nrow(Biomarker_data)==0){
+  if(ncol(RKD_data)==0 | nrow(RKD_data)==0){
     stop("You give an empty files")
   }
-
+  
   return(Biomarker_data)
 }
