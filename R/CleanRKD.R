363df0e80bca075a33c88c38c048b760b8f21f91
David Selby <david@selbys.org.uk>
2023-06-25 16:26:43 +0200

    Refactor rkd initial+encounter filter

--- R/CleanRKD.R
+++ R/CleanRKD.R
@@ -8,18 +8,16 @@
 #' @param rkd_data RKD data from \code{\link{load_rkd}} function
 #' @param ouput_path folder where the Redcap data will be saved
 #' @return The Redcap data cleaned in your folder and in an object
-#' The function change the Date variable with the format "%Y-%m-%d". 
+#' The function change the Date variable with the format "%Y-%m-%d".
 #' The function reduce the ethnicity to 6 group where different subgroup are regrouped.
-#' The function clean some RKD.ID trouble to be sure that we have no problem when we do any merge with other dataset 
+#' The function clean some RKD.ID trouble to be sure that we have no problem when we do any merge with other dataset
 #' The function select the variable present only for the Encounters data and the Initial data (demographics, diagnostics and all other exams performed at the moment of the diagnostics)and merge the Encounter dtaa and Initail data that the initial data are replicate for each Encounter
-#' The function create the following variable Age of the Encounter and ANCA titration (Anti MPO or Anti PR3 or NA) 
+#' The function create the following variable Age of the Encounter and ANCA titration (Anti MPO or Anti PR3 or NA)
 #' @import lubridate
 #' @import stringr
 #' @import dplyr
 #' @import forcats
 #' @export
-
-
 clean_rkd <- function(rkd_data, output_path) {
   # Check arguments
   stopifnot(is.data.frame(rkd_data))
@@ -31,113 +29,112 @@ clean_rkd <- function(rkd_data, output_path) {
   }
 
   # Parse all date columns
-  date_columns <- stringr::str_subset(colnames(rkd_data), 'Date')
-  date_columns <- stringr::str_subset(date_columns, 'known.unknown', negate = TRUE)
-  rkd_data <- dplyr::mutate(rkd_data,
-    dplyr::across(dplyr::all_of(date_columns), lubridate::as_date))
+  date_columns <- stringr::str_subset(colnames(rkd_data), "Date")
+  date_columns <- stringr::str_subset(date_columns, "known.unknown", negate = TRUE)
+  rkd_data <- dplyr::mutate(
+    rkd_data,
+    dplyr::across(dplyr::all_of(date_columns), lubridate::as_date)
+  )
 
   # Collapse ethnicities
-  rkd_data <- dplyr::mutate(rkd_data,
+  rkd_data <- dplyr::mutate(
+    rkd_data,
     dplyr::across(
-      dplyr::starts_with('Ethnicity'),
+      dplyr::starts_with("Ethnicity"),
       ~ forcats::fct_collapse(
-        stringr::str_extract(.x, '^[A-Z]{0,2}'),
-        White = 'W',
-        Asian = 'A',
-        Black = 'B',
-        `Mixed ethnicity` = 'M',
-        Other = 'O',
-        `Not Stated` = 'NS'
-      ))
-  
+        stringr::str_extract(.x, "^[A-Z]{0,2}"),
+        White = "W",
+        Asian = "A",
+        Black = "B",
+        `Mixed ethnicity` = "M",
+        Other = "O",
+        `Not Stated` = "NS"
+      )
+    )
+  )
+
   # Solve the problem of incorrect RKD IDs
   # If the RKD ID contains a '-', replace it with Patient Id
-  rkd_data$RKD.ID[stringr::str_detect(rkd_data$RKD.ID, '-')] <- NA
+  rkd_data$RKD.ID[stringr::str_detect(rkd_data$RKD.ID, "-")] <- NA
   if (any(is.na(rkd_data$RKD.ID))) {
-    warning('Replacing', paste(sum(is.na(rkd_data$RKD.ID)), 'incorrect RKD.IDs with Patient.Id'))
+    warning(
+      "Replacing ",
+      sum(is.na(rkd_data$RKD.ID)),
+      " incorrect RKD.IDs with Patient.ID"
+    )
+    rkd_data <- dplyr::mutate(rkd_data,
+      RKD.ID = dplyr::coalesce(
+        RKD.ID,
+        Patient.ID
+      )
+      # as.integer(Patient.Id))
+    )
   }
-  rkd_data <- dplyr::mutate(rkd_data, RKD.ID = dplyr::coalesce(RKD.ID, Patient.Id))
 
-  rkd_encounters <- dplyr::filter(rkd_data, Repeat.Instrument == 'Encounters')
-  rkd_initial <- dplyr::filter(rkd_data, Repeat.Instrument == '', Type.of.Patient != '')
 
+  rkd_encounters <- dplyr::filter(
+    rkd_data,
+    Repeat.Instrument == "Encounters"
+  )
+  rkd_initial <- dplyr::filter(
+    rkd_data,
+    Repeat.Instrument == "",
+    Type.of.Patient != ""
+  )
   # Select the variables of encounters
-  a_encounter_vars <- rkd_encounters %>%
-    dplyr::select(5:ncol(.)) %>%
-    dplyr::select(dplyr::where(~ all(is.na(.x)) | all(!nchar(.x)))) %>%
-    colnames()
+  id_columns <- c(
+    "RKD.ID", "Repeat.Instrument",
+    "Repeat.Instance", "Patient.Id"
+  )
+  # TODO: it is not necessary to store 'empty_encounters' or 'empty_initials'
+  # because we can just remove these columns inline using dplyr::where(!.)
 
-  b_encounter_vars <- setdiff(colnames(rkd_encounters)[-(1:4)], a_encounter_vars)  
-  
-  newdata <- RKD_Encounter[,1:4]
-  c=colnames(RKD_Encounter)
-  for( j in 1:length(b_Encounters)) {
-    newdata <- cbind(newdata, RKD_Encounter[,which(c == b_Encounters[j])])
-  }
-  colnames(newdata)[-c(1:4)] <- b_Encounters
-  RKD_Encounter_filter <- newdata
-  
-  a_Initials <- NULL
-  b_Initials <- NULL
-  for (i in 5:ncol(RKD_Initial)) {
-    # for-loop over columns
-    na_values <- length(which(is.na(RKD_Initial[,i]) == TRUE))
-    
-    if (na_values == nrow(RKD_Initial )) {
-      a_Initials <- c(a_Initials, colnames(RKD_Initial)[i])
-    }
-    else {
-      if(length(levels(as.factor(RKD_Initial[,i]))) == 1 & levels(as.factor(RKD_Initial[,i]))[1] == ""){
-        a_Initials <- c(a_Initials, colnames(RKD_Initial)[i])
-      }else{
-        b_Initials <- c(b_Initials, colnames(RKD_Initial)[i])
-      }
-      
-    }
-  }
+  # Get rid of empty / all-missing variables
+  rkd_encounter_filtered <- rkd_encounters %>%
+    dplyr::select(!dplyr::where(~ all(is.na(.x)) | all(!nchar(.x))))
+  rkd_initial_filtered <- rkd_initial %>%
+    dplyr::select(!dplyr::where(~ all(is.na(.x)) | all(!nchar(.x))))
 
-  
-  newdata <- RKD_Initial[,1:4]
-  c=colnames(RKD_Initial)
-  for( j in 1:length(b_Initials)) {
-    newdata <- cbind(newdata, RKD_Initial[,which(c == b_Initials[j])])
-  }
-  colnames(newdata)[-c(1:4)] <- b_Initials
-  RKD_Initial_filter <- newdata
-  
-  RKD_data_filter <- merge(RKD_Encounter_filter, RKD_Initial_filter[,-c(2:4)], by = "RKD.ID")
-  
-  RKD_data_filter$Age_Encounters <- year(RKD_data_filter$Date.Of.Visit)- year(RKD_data_filter$Date.of.Birth)
-  
+  # Merge together so we go from a sparse block-structure table
+  # to a wide table, effectively with initial values carried forward
+  # TODO: switch this to na.locf?
+  rkd_data_filtered <- rkd_encounter_filtered %>%
+    dplyr::full_join(rkd_initial_filtered %>%
+      dplyr::select(-Repeat.Instrument, -Repeat.Instance, -Patient.Id),
+    by = "RKD.ID"
+    )
+
+  RKD_data_filter$Age_Encounters <- year(RKD_data_filter$Date.Of.Visit) - year(RKD_data_filter$Date.of.Birth)
+
   RKD_data_filter$AntiMPO_PR3 <- NA
   m <- nrow(RKD_data_filter)
-  for(i in 1:m){
-    if (is.na(RKD_data_filter$Anti.MPO.level[i]) == TRUE & is.na(RKD_data_filter$Anti.PR3.level[i]) == TRUE){
+  for (i in 1:m) {
+    if (is.na(RKD_data_filter$Anti.MPO.level[i]) == TRUE & is.na(RKD_data_filter$Anti.PR3.level[i]) == TRUE) {
       RKD_data_filter$AntiMPO_PR3[i] <- NA
-    }else{
-      if(is.na(RKD_data_filter$Anti.MPO.level[i]) == TRUE & is.na(RKD_data_filter$Anti.PR3.level[i]) == FALSE){
+    } else {
+      if (is.na(RKD_data_filter$Anti.MPO.level[i]) == TRUE & is.na(RKD_data_filter$Anti.PR3.level[i]) == FALSE) {
         RKD_data_filter$AntiMPO_PR3[i] <- "PR3"
-      }else{
-        if(is.na(RKD_data_filter$Anti.MPO.level[i]) == FALSE & is.na(RKD_data_filter$Anti.PR3.level[i]) == TRUE){
+      } else {
+        if (is.na(RKD_data_filter$Anti.MPO.level[i]) == FALSE & is.na(RKD_data_filter$Anti.PR3.level[i]) == TRUE) {
           RKD_data_filter$AntiMPO_PR3[i] <- "MPO"
-        }else{
-          if(RKD_data_filter$Anti.MPO.level[i] > RKD_data_filter$Anti.PR3.level[i]){
+        } else {
+          if (RKD_data_filter$Anti.MPO.level[i] > RKD_data_filter$Anti.PR3.level[i]) {
             RKD_data_filter$AntiMPO_PR3[i] <- "MPO"
-          }else{
+          } else {
             RKD_data_filter$AntiMPO_PR3[i] <- "PR3"
           }
         }
       }
     }
   }
-  
+
   Clean_RKD_data <- RKD_data_filter
-  
-  files_test <-  list.dirs(output_path)
-  if(identical(files_test, character(0)) == TRUE){
+
+  files_test <- list.dirs(output_path)
+  if (identical(files_test, character(0)) == TRUE) {
     stop("Your output folder don't exist")
   }
   setwd(output_path)
-  write.csv(Clean_RKD_data, paste("Redcap_clinical_data_clean", Sys.Date() , ".csv", sep=""), row.names = F)
+  write.csv(Clean_RKD_data, paste("Redcap_clinical_data_clean", Sys.Date(), ".csv", sep = ""), row.names = F)
   return(Clean_RKD_data)
 }
