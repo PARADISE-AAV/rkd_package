8a65c17139923f743b1e170a0667d15bcf369771
MACTCD68 <COQM@tcd.ie>
2023-09-07 15:42:46 +0100

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -20,6 +20,7 @@ testthat::test_that("function LoadRKD", {
 })
 
 testthat::test_that("function cleanRKD", {
+  
   rkd=as.data.frame(NULL)
   output_path=1
   testthat::expect_error(clean_rkd(rkd, output_path), "Your argument need to be a character")
@@ -35,5 +36,6 @@ testthat::test_that("function cleanRKD", {
   rkd=as.data.frame(NULL)
   output_path=getwd()
   testthat::expect_error(clean_rkd(rkd, output_path), "You supplied an empty file")
+  
   
 }) 
