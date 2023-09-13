3b21c45c52883611e2b59753d8260bb97959e6f0
MACTCD68 <COQM@tcd.ie>
2023-09-13 15:44:48 +0100

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -73,4 +73,19 @@ testthat::test_that("function DemographicFilterRKD", {
   output_path=getwd()
   testthat::expect_error(DemographicFilterRKD(rkd, output_path), "You give an empty files")
   
+  output_path=getwd()
+  rkd=cbind.data.frame(RKD.ID=c(1,2,3,4,5,6),
+                       Small.vessel.vasculitis..ANCA.associated.=c("Granulomatosis with polyangiitis (Wegener) - Orpha:900","Granulomatosis with polyangiitis (Wegener) - Orpha:900","vasculitis","Microscopic polyangiitis (including renal limited vasculitis) - ORPHA:727","HV", "Microscopic polyangiitis (including renal limited vasculitis) - ORPHA:727"),
+                       Diagnosis.confidence=c("Definite", "Definite","", "probably", "maybe", "Definite"),
+                       Small.vessel.vasculitis..Immune.complex.=c("", "", "", "ANTI/GBM", "", ""),
+                       Secondary.vasculitis=c("No", "Yes","No", "Yes", "No", "No"),
+                       Other=c("No", "Yes","No", "Yes", "No", "No"),
+                       Medium.vessel.vasculitis=c("", "Kawasaky", "PAN", "", "", ""),
+                       Large.vessel.vasculitis= c("", "GCA", "Takayazu", "GCA", "", ""),
+                       Variable.vessel.vasculitis= c("", "", "Behcet", "", "Cogan", ""),
+                       At.any.point.ANCA.specificity=c("PR3", "MPO", "", "", "", ""),
+                       Biopsy.performed.=c("Yes", "no", "", "Yes", "No", "Yes"),
+                       Histologically.confirmed.diagnosis=c("No", "Yes", "No", "Yes", "Yes", "Yes"))
+  testthat::expect_equal(nrow(DemographicFilterRKD(rkd, output_path)), 2)
+  
 }) 
