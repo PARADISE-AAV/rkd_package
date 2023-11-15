a669fce3a415157940b490210dc8e57b6064bc8c
MACTCD68 <COQM@tcd.ie>
2023-11-15 13:26:41 +0000

    Update LoadBiomarker.R

--- R/LoadBiomarker.R
+++ R/LoadBiomarker.R
@@ -7,7 +7,8 @@
 #'
 #' @param files_name Biomarkerdata used
 #' @return The Biomarker Data into DataFrame
-#' @details The load of Biomarker data in a dataframe object is use to add the Biomarker data to the rkd data. Please mind that some verification of your files ad the date format can interrupt the function.
+#' @details The load of Biomarker data in a dataframe object is use to add the Biomarker data to the rkd data. 
+#' Please mind that some verification of your files ad the date format can interrupt the function.
 #' 
 #' 
 #' @import stringr
