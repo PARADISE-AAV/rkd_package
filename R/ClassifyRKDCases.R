abfa20bfd2553c184eef439d0177b788a991e93c
MACTCD68 <COQM@tcd.ie>
2023-11-15 08:01:44 +0000

    Update ClassifyRKDCases.R

--- R/ClassifyRKDCases.R
+++ R/ClassifyRKDCases.R
@@ -39,9 +39,8 @@ ClassifyRKDCases = function(RKDdata, output_path, algorithm, interval_from_diagn
     Classify_RKD_data <- Paradise_Encounter(RKD_data, interval_from_diagnostics)
   }
   if(algorithm == "CPD Relapse"){
-    JenRelapse_data <- JenRelapse(RKD_data)
-    Classify_RKD_data <- merge(RKD_data, JenRelapse_data, by.x = c("RKD.ID", "Date.Of.Visit"), by.y = c("ID", "Date.Of.Visit"))
-    
+    Classify_RKD_data <- JenRelapse(RKD_data)
+     
   }
 
   files_test <-  list.dirs(output_path)
