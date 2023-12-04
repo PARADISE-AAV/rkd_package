935098c660cb47c80035cff380a3630d82893eaa
MACTCD68 <COQM@tcd.ie>
2023-12-04 09:44:44 +0000

    Update JenRelapse.R

--- R/JenRelapse.R
+++ R/JenRelapse.R
@@ -7,7 +7,7 @@
 #'
 #' @param RKDdata Data frame with the RKD data in
 #' @details The CPD relapse function have the objective to tell if an Encounter is in Relapse or not
-#' The algorithm is described in the following picture https://3.basecamp.com/3790396/buckets/31062049/uploads/6740235559
+#' The algorithm is described in the following picture * https://3.basecamp.com/3790396/buckets/31062049/uploads/6740235559
 #' There is 4 sub-function
 #' prep1 and prep2 are function to prepare the data for the modelling and the classification
 #' cpd.relapse is the function to apply the model described in Jennifer Scott article
