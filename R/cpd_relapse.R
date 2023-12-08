6806a659b6687232fcd285fa92d21ce077bc51f3
MACTCD68 <COQM@tcd.ie>
2023-12-08 10:31:43 +0000

    warning solving

--- R/cpd_relapse.R
+++ R/cpd_relapse.R
@@ -43,10 +43,10 @@ cpd.relapse <- function (RKDdata){
                             "Discontinuation of immunosuppression within 6 months prior to this encounter", NA))
   set.seed(9582)
   
-  model_master_list <- readRDS(file = system.file("data/model_master_list_17.1.23.rds",package = "rkdpipeline"))
+  model_master_list <- readRDS(file = system.file("extdata/model_master_list_17.1.23.rds",package = "rkdpipeline"))
   
   
-  cut_point_master <- readRDS(file = system.file("data/cut_point_master_17.1.23.rds",package = "rkdpipeline"))
+  cut_point_master <- readRDS(file = system.file("extdata/cut_point_master_17.1.23.rds",package = "rkdpipeline"))
   model_ind <- expand.grid(c(0,1), c(0, 1), c(0, 1), c(0, 1), c(0, 1)) %>% apply(1, as.logical) %>% t
   model_ind <- model_ind[-1, ]
   id <- dat %>% 
