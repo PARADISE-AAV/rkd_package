b9eeaa2a6ce29e1cf27f6efea64a369c2e762cb2
MACTCD68 <COQM@tcd.ie>
2023-12-08 15:38:08 +0000

    Update prep2.R

--- R/prep2.R
+++ R/prep2.R
@@ -47,10 +47,11 @@ prep2 <- function(RKDdata){
 #'
 #' @param RKDdata Data frame with the RKD data in
 #' @return The RKD data after the second part of the data preparation
+#' @import dplyr
 #' @export
 
 inclusion_criteria <- function(RKDdata){
-  library(dplyr)
+  
   ####Test on the argument
   if (is.data.frame(RKDdata) == FALSE) {
     stop("The argument RKDdata need to be a dataframe argument")
