b6891dafe2ff622b6799de63ceba89c0d4105a76
MACTCD68 <COQM@tcd.ie>
2023-12-18 11:16:05 +0000

    Update ClassifyRKDCases.R

--- R/ClassifyRKDCases.R
+++ R/ClassifyRKDCases.R
@@ -8,6 +8,7 @@
 #' @param output_path folder where the Redcap data will be saved
 #' @param algorithm function use to classify the RKD patient, the possibility are "BRelapse" or "Paradise_Encounter" or "CPD Relapse"
 #' @param interval_from_diagnostics the interval from diagnostics for the algorithm Paradise_Encounter by default 6
+#' @param rawRKDdata Raw data from 
 #' @return The Redcap data cleaned in your folder and in an object 
 #' @details This function use different function to classify the encounter of RKD data. this function are described in their function.
 #' * CPD Relapse tells us if an encounter is in relapse or not based on rules and models 
@@ -15,7 +16,7 @@
 #' * Paradise_Encounter tells us if we can include the Encounter or not in the Paradise project
 #' 
 #' @export
-ClassifyRKDEncounter = function(RKDdata, output_path, algorithm, interval_from_diagnostics=6) {
+ClassifyRKDEncounter = function(RKDdata, output_path, algorithm, interval_from_diagnostics=6, rawRKDdata) {
   ####Test on the argument
   if (is.data.frame(RKDdata) == FALSE) {
     stop("The argument RKDdata need to be a dataframe argument")
@@ -40,6 +41,10 @@ ClassifyRKDEncounter = function(RKDdata, output_path, algorithm, interval_from_d
   if(algorithm == "CPD Relapse"){
     Classify_RKD_data <- CPD_Relapse(RKD_data)
      
+  }
+  if(algorithm == "Treatment On/Off"){
+    rkd_treatment <- Treatment_On_Off(RKDdata)
+    Classify_RKD_data <- rkdpipeline:::IVTherapy(rkd, rkd_treatment)
   }
 
   files_test <-  list.dirs(output_path)
