a39c443ebc75140f89e9c1a40016e16480b5c0ab
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2024-01-02 10:25:35 +0000

    Update DemographicFilterRKD.R

--- R/DemographicFilterRKD.R
+++ R/DemographicFilterRKD.R
@@ -75,7 +75,7 @@ DemographicFilterRKD <- function(RKDdata, output_path) {
     stop("Your output folder don't exist")
   }
 
-  output_filename <- file.path(output_path, paste0("Redcap_clinical_data_filter", Sys.Date() , ".csv"))
+  output_filename <- file.path(output_path, paste0("Redcap_clinical_data_filter", "_version", packageVersion('rkdpipeline'), "_Date", Sys.Date() , ".csv"))
   write.csv(Filter_RKD_data, output_filename, row.names = FALSE)
 
   return(Filter_RKD_data)
