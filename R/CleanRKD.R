5d57b5c2fc5caf4cac89f06cbc9539354524b519
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2024-01-04 08:26:11 +0000

    Update CleanRKD.R

--- R/CleanRKD.R
+++ R/CleanRKD.R
@@ -10,7 +10,7 @@
 #' @param rkd_data RKD data from \code{\link{load_rkd}} function
 #' @param output_path folder where the Redcap data will be saved
 #' @details
-#' The Redcap data cleaned in your folder and in an object
+#' The Redcap data cleaned in your folder and in an R object
 #' 
 #' The function change the Date variable with the format "%Y-%m-%d".
 #' 
