f008d84b8610665590dd6d08f45ded1e05128c5a
MACTCD68 <COQM@tcd.ie>
2024-01-22 09:10:40 +0000

    Update ClassifyRIVCases.R

--- R/ClassifyRIVCases.R
+++ R/ClassifyRIVCases.R
@@ -60,7 +60,7 @@ ClassifyRIVEncounter = function(RKDdata, output_path, algorithm, interval_from_d
   }
 
   output_filename <- file.path(output_path,
-    paste0("Redcap_clinical_data_with-classification", "_version", packageVersion('rkdpipeline'), "_Date", Sys.Date(), ".csv"))
+    paste0("Redcap_clinical_data_with-classification", "_version", packageVersion('rivpipeline'), "_Date", Sys.Date(), ".csv"))
 
   write.csv(Classify_RKD_data, output_filename, row.names = FALSE)
   return(Classify_RKD_data)
