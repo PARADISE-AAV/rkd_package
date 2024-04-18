ab80db62e9254007557dd131467953f856989fb4
MACTCD68 <COQM@tcd.ie>
2024-04-18 09:47:16 +0100

    Update CPD_Encounter.R

--- R/CPD_Encounter.R
+++ R/CPD_Encounter.R
@@ -17,7 +17,7 @@
 #' @import forcats
 #' @importFrom rlang .data
 #' @export
-CPD_Continuous_Medication <- function (Encounter_data, output_dir){
+CPD_Encounter <- function (Encounter_data, output_dir){
   stopifnot("Your argument need to be a data frame"=is.data.frame(Encounter_data))
   stopifnot("Your argument need to be a character"=is.character(output_dir))
   
