8c08da8c9fee6672d420488d27d7cf7f3a876c39
MACTCD68 <COQM@tcd.ie>
2024-05-09 12:47:24 +0100

    Create CPD_IS_Imputation.R

---
+++ R/CPD_IS_Imputation.R
@@ -0,0 +1,33 @@
+#' @title CPD IS Imputation
+#' @author Matthieu COQ
+#'
+#' @description The Goal is to do CPD IS Imputation for Treatment On/Off
+#' 
+#' Version: 1.0
+#' 
+#' Date: 9-May-23
+#'
+#' @param Encounter Data from Encounter from \code{\link{CPD_Encounter}} function
+#' @param output_dir folder where the Redcap data will be saved
+#' @details
+#' to be added
+#' 
+#' @import lubridate
+#' @export
+
+CPD_IS_Imputation <- function(Encounter, output_dir){
+  
+  stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
+  stopifnot("Your argument need to be a character"=is.character(output_dir))
+  
+  # Check output directory
+  if (!dir.exists(output_dir)) {
+    stop('Specified output folder does not exist')
+  }
+  
+  n=nrow(Encounter)-1
+  for(i in 2:n){
+    if((Encounter$RKD.ID[i-1])==)
+  }
+  
+}
