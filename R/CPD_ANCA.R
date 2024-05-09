0dd7f3b4846baffe68afa003a7c022c873201520
MACTCD68 <COQM@tcd.ie>
2024-05-09 10:24:06 +0100

    Update CPD_ANCA.R

--- R/CPD_ANCA.R
+++ R/CPD_ANCA.R
@@ -41,15 +41,16 @@ CPD_ANCA <- function(merge_data, output_dir){
   rkd$ANCA_Status="ANCA Status Unknown"
   n=nrow(rkd)
   for(i in 1:n){
-    if(( is.na(rkd$ANCA_Levels[i])== FALSE & rkd$ANCA_Levels[i] > 2) | rkd$ANCA.IF[i] == "Atypical" | rkd$ANCA.IF[i]  == "P" | rkd$ANCA.IF[i]  == "C"){
-      rkd$ANCA_Statuts[i] = "ANCA Positive"
+    
+    if(( is.na(rkd$ANCA_Levels[i])== FALSE & as.numeric(rkd$ANCA_Levels[i]) > 2 & rkd$ANCA_Levels[i] != "Unknown" ) | rkd$ANCA.IF[i] == "Atypical" | rkd$ANCA.IF[i]  == "P" | rkd$ANCA.IF[i]  == "C"){
+      rkd$ANCA_Status[i] = "ANCA Positive"
     }
-    if(( is.na(rkd$ANCA_Levels[i])== FALSE & rkd$ANCA_Levels[i] <= 2) | rkd$ANCA.IF[i] == "Negative" ){
-      rkd$ANCA_Statuts[i] = "ANCA Negative"
+    if(( is.na(rkd$ANCA_Levels[i])== FALSE & as.numeric(rkd$ANCA_Levels[i]) <= 2 & rkd$ANCA_Levels[i] != "Unknown" ) | rkd$ANCA.IF[i] == "Negative" ){
+      rkd$ANCA_Status[i] = "ANCA Negative"
     }
     
   }
-  rkd$ANCA_Switch = "Switch status Unknown"
+  rkd$ANCA_Switch = NA
   n=nrow(rkd)
   for( i in 2:n){
     if(rkd$RKD.ID[i] == rkd$RKD.ID[i-1] & interval(rkd$Date.Of.Visit[i-1], rkd$Date.Of.Visit[i]) %/% months(1)<=18 & interval(rkd$Date.Of.Visit[i-1], rkd$Date.Of.Visit[i]) %/% months(1)>=1 &  (is.na(rkd$CPD_relapse[i])== FALSE & rkd$CPD_relapse[i] == "No Relapse") & (is.na(rkd$CPD_relapse[i-1])== FALSE & rkd$CPD_relapse[i-1] == "No Relapse")){
@@ -65,7 +66,14 @@ CPD_ANCA <- function(merge_data, output_dir){
       if(rkd$ANCA_Status[i-1] == "ANCA Positive" & rkd$ANCA_Status[i] == "ANCA Negative"){
         rkd$ANCA_Switch[i] = "Pos-Neg Switch"
       }
-      
+      if((rkd$ANCA_Status[i-1] == "ANCA Status Unknown" & rkd$ANCA_Status[i] == "ANCA Negative") | 
+         (rkd$ANCA_Status[i-1] == "ANCA Status Unknown" & rkd$ANCA_Status[i] == "ANCA Positive") |
+         (rkd$ANCA_Status[i-1] == "ANCA Negative" & rkd$ANCA_Status[i] == "ANCA Status Unknown") | 
+         (rkd$ANCA_Status[i-1] == "ANCA Positive" & rkd$ANCA_Status[i] == "ANCA Status Unknown") |
+         (rkd$ANCA_Status[i-1] == "ANCA Status Unknown" & rkd$ANCA_Status[i] == "ANCA Status Unknown")
+         ){
+        rkd$ANCA_Switch[i] = "Switch Status Unknown"
+      }
     }
   }
   
