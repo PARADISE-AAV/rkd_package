5c6a1e845a98c56b96ed21d8bc9437664c532b09
MACTCD68 <COQM@tcd.ie>
2024-05-15 11:56:19 +0100

    Create CPD_IVTherapy_Treatment.R

---
+++ R/CPD_IVTherapy_Treatment.R
@@ -0,0 +1,20 @@
+#' @title CPD IV Therapy treatment
+#' @author Matthieu COQ
+#'
+#' @description The Goal is to do CPD IS Imputation for Treatment On/Off
+#' 
+#' Version: 1.0
+#' 
+#' Date: 9-May-23
+#'
+#' @param IVTherapy Data from Encounter from \code{\link{CPD_IVTherapy}} function
+#' @param output_dir folder where the Redcap data will be saved
+#' @details
+#' to be added
+#' 
+#' @import lubridate
+#' @export
+
+CPD_IVTherapy_Treatment= function(IVTherapy, output_dir){
+  
+}
