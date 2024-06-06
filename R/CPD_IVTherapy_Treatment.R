65121185551a317dfd3c109176054517609c432b
MACTCD68 <COQM@tcd.ie>
2024-06-06 11:12:42 +0100

    Update CPD_IVTherapy_Treatment.R

--- R/CPD_IVTherapy_Treatment.R
+++ R/CPD_IVTherapy_Treatment.R
@@ -21,7 +21,7 @@
 
 CPD_IVTherapy_Treatment= function(IV_Therapy, merged_data, output_dir){
   
-  stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
+  stopifnot("Your argument need to be a data frame"=is.data.frame(merged_data))
   stopifnot("Your argument need to be a data frame"=is.data.frame(IV_Therapy))
   stopifnot("Your argument need to be a character"=is.character(output_dir))
   
