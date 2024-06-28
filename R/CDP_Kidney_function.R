16bd16712858eea56e5d815f50d6b053e290906d
MACTCD68 <COQM@tcd.ie>
2024-06-28 14:31:56 +0100

    Create CDP_Kidney_function.R

---
+++ R/CDP_Kidney_function.R
@@ -0,0 +1,24 @@
+#' @title CPD Kidney function
+#' @author Matthieu COQ
+#'
+#' @description The Goal is to merge the Continuous medication and general characteristics from \code{\link{SplitRIV}} function
+#' 
+#' Version: 1.0
+#' 
+#' Date: 17-Apr-23
+#'
+#' @param renal list of data frame from \code{\link{SplitRIV}} function
+#' @param output_dir folder where the Redcap data will be saved
+#' @details
+#' to be added
+#' @import lubridate
+#' @import stringr
+#' @import dplyr
+#' @import forcats
+#' @importFrom rlang .data
+#' @export
+
+CPD_Kidney-function <- function (renal, output_dir){
+  
+  
+}
