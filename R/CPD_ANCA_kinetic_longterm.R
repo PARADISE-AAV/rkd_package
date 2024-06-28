9b710a75ee80cc2b536062c3177a56b31f1fdb74
MACTCD68 <COQM@tcd.ie>
2024-06-28 09:03:52 +0100

    Update CPD_ANCA_kinetic_longterm.R

--- R/CPD_ANCA_kinetic_longterm.R
+++ R/CPD_ANCA_kinetic_longterm.R
@@ -7,7 +7,7 @@
 #' 
 #' Date: 9-May-23
 #'
-#' @param merged_data Data from the merge of encounter and General characteristics in the \code{\link{Merge_Encounter_initial}} function
+#' @param merged_data Data from the \code{\link{CPD_ANCA}} function
 #' @param output_dir folder where the Redcap data will be saved
 #' @details
 #' to be added
