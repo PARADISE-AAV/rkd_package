6ee08ffe9d3a35120c6abdb5ceb263b480352595
MACTCD68 <COQM@tcd.ie>
2024-07-10 09:59:47 +0100

    Update CDP_Kidney_function.R

--- R/CDP_Kidney_function.R
+++ R/CDP_Kidney_function.R
@@ -33,6 +33,33 @@ CPD_Kidney_function <- function (merge_data, renal, output_dir){
   renal_frame <- merge(renal, merge_data[, c("RKD.ID", "Date_Last_Follow_up")], by= "RKD.ID")
   renal_frame <- renal_frame %>% distinct(RKD.ID, Date.of.transplant., .keep_all=TRUE)
   
+  renal_frame <- renal_frame %>% 
+    dplyr::mutate(Date.of.graft.failure = dplyr::case_when(
+      is.na(Date.of.graft.failure) == TRUE ~ Date_Last_Follow_up,
+      is.na(Date.of.graft.failure) == FALSE ~ Date.of.graft.failure
+    ))
+  
+  
+  merged_frame <-fuzzy_inner_join(
+    merge_data, renal_frame,
+    by = c(
+      "RKD.ID" = "RKD.ID",
+      "Date.Of.Visit" = "Date.of.transplant.",
+      "Date.Of.Visit" = "Date.of.graft.failure" 
+    ),
+    match_fun = list(`==`, `>=`, `<=`)
+  ) %>%
+    select(everything())
+  
+  
+  rownames(merged_frame) <- NULL
+  
+  merged_frame$Dialysis = "Transplant"
+  
+  colnames(merged_frame)[1] = "RKD.ID"
+  
+  merge_renal_frame = merge(merge_data, merged_frame[,c("RKD.ID", "Date.Of.Visit", "Dialysis")], by= c("RKD.ID", "Date.Of.Visit"),all.x = TRUE)
+  
   
   output_filename <- file.path(
     output_dir,
