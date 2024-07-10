23ac8a09a0afc53edca1df8e4164859142c1a358
MACTCD68 <COQM@tcd.ie>
2024-07-10 11:29:56 +0100

    Create CPD_LTROT_current.R

---
+++ R/CPD_LTROT_current.R
@@ -0,0 +1,40 @@
+#' @title CPD current LTROT
+#' @author Matthieu COQ
+#'
+#' @description to be added
+#' 
+#' Version: 1.0
+#' 
+#' Date: 17-Apr-23
+#'
+#' @param merge_data Data from the merge of encounter and General characteristics in the \code{\link{Merge_Encounter_initial}} function
+#' @param output_dir folder where the Redcap data will be saved
+#' @details
+#' to be added
+#' @import lubridate
+#' @import stringr
+#' @import dplyr
+#' @import forcats
+#' @importFrom rlang .data
+#' @export
+
+CPD_LTROT_current <- function(merge_data, output_dir){
+  
+  stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
+  stopifnot("Your argument need to be a character"=is.character(output_dir))
+  
+  # Check output directory
+  if (!dir.exists(output_dir)) {
+    stop('Specified output folder does not exist')
+  }
+  
+  
+  output_filename <- file.path(
+    output_dir,
+    paste0('Redcap_ltrot_current_function_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
+           , Sys.Date(), '.csv')
+  )
+  write.csv(rkd_data, output_filename, row.names = FALSE)
+  return(rkd_data)
+  
+}
