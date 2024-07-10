e8a8c686ee7f9f61373a2e01aa9362a86c0b1409
MACTCD68 <COQM@tcd.ie>
2024-07-10 14:39:38 +0100

    Update CPD_Treatment_OnOff.R

--- R/CPD_Treatment_OnOff.R
+++ R/CPD_Treatment_OnOff.R
@@ -75,21 +75,21 @@ CPD_Treatment_OnOff= function(IV_Therapy, ConMed, merged_data, output_dir){
   data_merged <- data_merged %>%
     dplyr::mutate(Step5 = dplyr::case_when(
       Immunosuppressive.medication == "No" ~ "Off treatment",
-      Immunosuppressive.medication != "No" | Immunosuppressive.medication != "" ~ "On treatment"
+      Immunosuppressive.medication != "No" & Immunosuppressive.medication != "" ~ "On treatment"
     ))
   
   data_merged$Step6 <- NA
   n=nrow(data_merged)
   for (i in 1:n){
-    if(is.na(data_merged$`Drug_Prednisolone - UATC/H02AB06`[i]) == FALSE & (is.na(data_merged$`Dose_Prednisolone - UATC/H02AB06`[i]) == FALSE & data_merged$`Dose_Prednisolone - UATC/H02AB06`[i] <= 10)){
+    if(is.na(data_merged$`Dose_Prednisolone - UATC/H02AB06`[i]) == FALSE & data_merged$`Dose_Prednisolone - UATC/H02AB06`[i] <= 10){
       data_merged$Step6[i]="Prednisolone<=10"
     }
-    if(is.na(data_merged$`Drug_Prednisolone - UATC/H02AB06`[i]) == FALSE & (is.na(data_merged$`Dose_Prednisolone - UATC/H02AB06`[i]) == FALSE & data_merged$`Dose_Prednisolone - UATC/H02AB06`[i] > 10)){
+    if(is.na(data_merged$`Dose_Prednisolone - UATC/H02AB06`[i]) == FALSE & data_merged$`Dose_Prednisolone - UATC/H02AB06`[i] > 10){
       data_merged$Step6[i]="On treatment"
     }
-    if(is.na(data_merged$`Drug_Avacopan (C5aR inhibitor)`[i]) == FALSE | is.na(data_merged$`Drug_Azathioprine - UATC/L04AX01`[i]) == FALSE | is.na(data_merged$`Drug_Cyclophosphamide - UATC/L01AA01`[i]) == FALSE 
+    if((is.na(data_merged$`Drug_Avacopan (C5aR inhibitor)`[i]) == FALSE | is.na(data_merged$`Drug_Azathioprine - UATC/L04AX01`[i]) == FALSE | is.na(data_merged$`Drug_Cyclophosphamide - UATC/L01AA01`[i]) == FALSE 
        | is.na(data_merged$`Drug_Methotrexate - UATC/L01BA01`[i]) == FALSE | is.na(data_merged$`Drug_Mycophenolate mofetil - UATC/L04AA06`[i]) == FALSE 
-       | is.na(data_merged$`Drug_Other`[i]) == FALSE){
+       | is.na(data_merged$`Drug_Other`[i]) == FALSE) & is.na(data_merged$Step6[i])==TRUE){
       data_merged$Step6[i]="On treatment"
     }
     if(is.na(data_merged$`Drug_Avacopan (C5aR inhibitor)`[i]) == TRUE & is.na(data_merged$`Drug_Azathioprine - UATC/L04AX01`[i]) == TRUE & is.na(data_merged$`Drug_Cyclophosphamide - UATC/L01AA01`[i]) == TRUE 
@@ -102,7 +102,7 @@ CPD_Treatment_OnOff= function(IV_Therapy, ConMed, merged_data, output_dir){
   data_merged$CPD_treatment <- "Manual review"
   n=nrow(data_merged)
   for (i in 1:n){
-    if(is.na(data_merged$Step1[i]) == TRUE & (is.na(data_merged$Step2[i]) == FALSE | is.na(data_merged$Step3[i]) == FALSE | data_merged$Step4[i] == "On treatment"| data_merged$Step5[i] == "On treatment"| data_merged$Step6[i] == "On treatment" | is.na (data_merged$Step4[i])==TRUE)){
+    if(is.na(data_merged$Step1[i]) == TRUE & (is.na(data_merged$Step2[i]) == FALSE | is.na(data_merged$Step3[i]) == FALSE | data_merged$Step4[i] == "On treatment"| (is.na(data_merged$Step5[i])==FALSE & data_merged$Step5[i] == "On treatment")| data_merged$Step6[i] == "On treatment" | is.na (data_merged$Step4[i])==TRUE)){
       data_merged$CPD_treatment[i]="On Treatment"
     }
     if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE & is.na(data_merged$Step3[i]) == TRUE & is.na(data_merged$Step4[i]) == TRUE & is.na(data_merged$Step5[i]) == TRUE & is.na(data_merged$Step6[i]) == TRUE){
