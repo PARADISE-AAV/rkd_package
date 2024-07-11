3e6c7cd76b0bfd4fa9743ffa8f291e3706617382
MACTCD68 <COQM@tcd.ie>
2024-07-11 12:39:23 +0100

    Update CPD_Treatment_OnOff.R

--- R/CPD_Treatment_OnOff.R
+++ R/CPD_Treatment_OnOff.R
@@ -114,6 +114,9 @@ CPD_Treatment_OnOff= function(IV_Therapy, ConMed, merged_data, output_dir){
     if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == FALSE & is.na(data_merged$Step3[i])==TRUE & (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "On treatment") & (is.na(data_merged$Step5[i])==TRUE | data_merged$Step5[i] == "On treatment")){
       data_merged$CPD_treatment[i]="On Treatment"
     }
+    if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == FALSE & is.na(data_merged$Step3[i])==TRUE & (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "On treatment") & (is.na(data_merged$Step5[i])==FALSE & data_merged$Step5[i] == "Off treatment")){
+      data_merged$CPD_treatment[i]="Manual review"
+    }
   }
   
   data_merged$IVtherapy=as.character(data_merged$IVtherapy)
