ef25ebce4d9a0bca08a0f8c21676d4a16d42039b
MACTCD68 <COQM@tcd.ie>
2024-07-11 15:43:33 +0100

    Update CPD_Treatment_OnOff.R

--- R/CPD_Treatment_OnOff.R
+++ R/CPD_Treatment_OnOff.R
@@ -99,7 +99,7 @@ CPD_Treatment_OnOff= function(IV_Therapy, ConMed, merged_data, output_dir){
     }
   }
   
-  data_merged$CPD_treatment <- ""
+  data_merged$CPD_treatment <- "Manual review"
   n=nrow(data_merged)
   for (i in 1:n){
     if(is.na(data_merged$Step1[i]) == TRUE  & (is.na(data_merged$Step4[i])==TRUE | data_merged$Step4[i] == "On treatment") & (is.na(data_merged$Step5[i])==T | data_merged$Step5[i] == "On treatment")){
@@ -119,6 +119,9 @@ CPD_Treatment_OnOff= function(IV_Therapy, ConMed, merged_data, output_dir){
     }
     if (is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE & is.na(data_merged$Step3[i]) == TRUE & (is.na(data_merged$Step4[i])==TRUE ) & (is.na(data_merged$Step5[i])==TRUE) & data_merged$Step6[i] == "Treatment Status Unknown"){
       data_merged$CPD_treatment[i]="Treatment Status Unknown"
+    }
+    if (is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE & is.na(data_merged$Step3[i]) == TRUE & (is.na(data_merged$Step4[i])==TRUE ) & (is.na(data_merged$Step5[i])==TRUE | data_merged$Step5[i] == "Off treatment") & data_merged$Step6[i] == "Prednisolone<=10"){
+      data_merged$CPD_treatment[i]="Prednisolone<=10"
     }
   }
   
