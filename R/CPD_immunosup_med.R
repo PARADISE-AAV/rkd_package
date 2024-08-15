4e480d07a87da67c1534904284be3f9445a3a869
MACTCD68 <COQM@tcd.ie>
2024-08-15 10:53:36 +0100

    Create CPD_immunosup_med.R

---
+++ R/CPD_immunosup_med.R
@@ -0,0 +1,77 @@
+#' @title CPD IS medication
+#' @author Matthieu COQ
+#'
+#' @description The Goal is to do CPD Treatment
+#' 
+#' Version: 1.0
+#' 
+#' Date: 9-Aug-24
+#'
+#' @param treatment_data Data from CPD_treatment from \code{\link{CPD_Treatment}} function
+#' @param output_dir folder where the Redcap data will be saved
+#' @details
+#' to be added
+#' 
+#' @import lubridate
+#' @import DT
+#' @import dplyr
+#' @import fuzzyjoin
+#' @importFrom data.table %like%
+#' @export
+
+CPD_immunosup_med <- function(treatment_data, output_dir){
+  
+  stopifnot("Your argument need to be a data frame"=is.data.frame(treatment_data))
+  stopifnot("Your argument need to be a character"=is.character(output_dir))
+  
+  # Check output directory
+  if (!dir.exists(output_dir)) {
+    stop('Specified output folder does not exist')
+  }
+  
+  treatment_data$immunosup_med_1="No"
+  treatment_data$immunosup_med_1[grep("Cyclo", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_2="No"
+  treatment_data$immunosup_med_2[grep("Mycophenolate", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_3="No"
+  treatment_data$immunosup_med_3[grep("Azathioprine", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_4="No"
+  treatment_data$immunosup_med_4[grep("Methotrexate", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_5="No"
+  treatment_data$immunosup_med_5[grep("Leflunomide", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_6="No"
+  treatment_data$immunosup_med_6[grep("Rituximab", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_7="No"
+  treatment_data$immunosup_med_7[grep("Ustekinumab", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_8="No"
+  treatment_data$immunosup_med_8[grep("Tacrolimus", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_9="No"
+  treatment_data$immunosup_med_9[grep("Mepolizumab", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_10="No"
+  treatment_data$immunosup_med_10[grep("Avacopan", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_11="No"
+  treatment_data$immunosup_med_11[grep("Other", treatment_data$treatment)]="Yes"
+  
+  treatment_data$immunosup_med_12="No"
+  treatment_data$immunosup_med_12[which(is.na(treatment_data$treatment)==TRUE)]="Yes"
+  
+  
+  output_filename <- file.path(
+    output_dir,
+    paste0('Redcap_CPDTreatment_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
+           , Sys.Date(), '.csv')
+  )
+  write.csv(treatment_data, output_filename, row.names = FALSE)
+  return(treatment_data)
+  
+}
