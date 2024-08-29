5b275f65a28edf72f0f9b2bf0b41c5644a3bba0b
MACTCD68 <COQM@tcd.ie>
2024-08-29 09:33:10 +0100

    Create CPD_Corticosteroids_on_off.R

---
+++ R/CPD_Corticosteroids_on_off.R
@@ -0,0 +1,38 @@
+#' @title CPD Corticosteroids on/off
+#' @author Matthieu COQ
+#'
+#' @description to be added
+#' 
+#' Version: 1.0
+#' 
+#' Date: 15-Aug-23
+#'
+#' @param merge_data Data from the merge of encounter and General characteristics in the \code{\link{CPD_Treatment}} function
+#' @param output_dir folder where the Redcap data will be saved
+#' @details
+#' to be added
+#' @import lubridate
+#' @import dplyr
+#' @export
+
+CPD_Corticosteroids_on_off <- function(merge_data, output_dir){
+  
+  stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
+  stopifnot("Your argument need to be a character"=is.character(output_dir))
+  
+  # Check output directory
+  if (!dir.exists(output_dir)) {
+    stop('Specified output folder does not exist')
+  }
+  
+  
+  
+  output_filename <- file.path(
+    output_dir,
+    paste0('Redcap_ltrot_current_function_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
+           , Sys.Date(), '.csv')
+  )
+  write.csv(merge_LTROT, output_filename, row.names = FALSE)
+  return(merge_LTROT)
+  
+}
