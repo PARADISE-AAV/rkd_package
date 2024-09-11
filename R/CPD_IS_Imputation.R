ce51f7fc56b9b0cbdbe46ad00349e9b85b9029e4
MACTCD68 <COQM@tcd.ie>
2024-09-11 15:11:22 +0100

    Update CPD_IS_Imputation.R

--- R/CPD_IS_Imputation.R
+++ R/CPD_IS_Imputation.R
@@ -26,22 +26,41 @@ CPD_IS_Imputation <- function(Encounter, output_dir){
   }
   
   n=length(levels(as.factor(Encounter$RKD.ID)))
+  Encounter2=NULL
   for(i in 1:n){
     dat=Encounter[which(Encounter$RKD.ID==levels(as.factor(Encounter$RKD.ID))[i]),]
     m=nrow(dat)
-    for (j in 2:m){
-      if(dat$Immunosuppressive.medication[j]==""){
-        if(dat$Immunosuppressive.medication[j-1]!="" & j!=m){
-          if(dat$Immunosuppressive.medication[j+1]!="" & j+1!=m){
-            
-          }else{
-            
+    if(m>3){
+      for (j in 2:m){
+        if(dat$Immunosuppressive.medication[j]==""){
+          if(dat$Immunosuppressive.medication[j-1]!="" & j<=m){
+            if(dat$Immunosuppressive.medication[j+1]!="" & j+1<=m){
+              if(dat$Immunosuppressive.medication[j+1] == dat$Immunosuppressive.medication[j-1] & 
+                 as.numeric(dat$Date.Of.Visit[j+1]-dat$Date.Of.Visit[j-1])<=730){
+                dat$Immunosuppressive.medication[j]=dat$Immunosuppressive.medication[j-1]
+              }
+            }else{
+              if(dat$Immunosuppressive.medication[j+2]!="" & j+2<=m){
+                if(dat$Immunosuppressive.medication[j+2] == dat$Immunosuppressive.medication[j-1]& 
+                   as.numeric(dat$Date.Of.Visit[j+2]-dat$Date.Of.Visit[j-1])<=730){
+                  dat$Immunosuppressive.medication[j]=dat$Immunosuppressive.medication[j-1]
+                }
+              }else{
+              if(dat$Immunosuppressive.medication[j+3]!="" & j+3<=m){
+                if(dat$Immunosuppressive.medication[j+3] == dat$Immunosuppressive.medication[j-1]& 
+                   as.numeric(dat$Date.Of.Visit[j+3]-dat$Date.Of.Visit[j-1])<=730){
+                  dat$Immunosuppressive.medication[j]=dat$Immunosuppressive.medication[j-1]
+                }
+              }
+            }
           }
         }
       }
     }
-  }
-  
+    
+    }
+    Encounter2=rbind(Encounter2,dat)
+    } 
   
   output_filename <- file.path(
     output_dir,
