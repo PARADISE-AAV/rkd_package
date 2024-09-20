d5f2784f5be2685cd7da28a8beff157926122d42
MACTCD68 <COQM@tcd.ie>
2024-09-20 12:39:28 +0100

    Update CPD_LTROT_current.R

--- R/CPD_LTROT_current.R
+++ R/CPD_LTROT_current.R
@@ -8,7 +8,6 @@
 #' Date: 15-Aug-23
 #'
 #' @param merge_data Data from the merge of encounter and General characteristics in the \code{\link{CPD_Treatment}} function
-#' @param output_dir folder where the Redcap data will be saved
 #' @param interval Number of day to be off treatment
 #' @details
 #' to be added
@@ -16,16 +15,12 @@
 #' @import dplyr
 #' @export
 
-CPD_LTROT_current <- function(merge_data, interval, output_dir){
+CPD_LTROT_current <- function(merge_data, interval){
   
   stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
-  stopifnot("Your argument need to be a character"=is.character(output_dir))
   stopifnot("Your argument need to be a numeric"=is.numeric(interval))
   
-  # Check output directory
-  if (!dir.exists(output_dir)) {
-    stop('Specified output folder does not exist')
-  }
+  
   
   merge_data1=merge_data[which(is.na(merge_data$CPD_relapse)==FALSE),]
   merge_data1$LTROT_current=NA
@@ -55,12 +50,7 @@ CPD_LTROT_current <- function(merge_data, interval, output_dir){
   
   merge_LTROT=merge(merge_data, merge_data_LTROT, by = c("RKD.ID", "Date.Of.Visit"), all.x = TRUE)
   
-  output_filename <- file.path(
-    output_dir,
-    paste0('Redcap_ltrot_current_function_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
-           , Sys.Date(), '.csv')
-  )
-  write.csv(merge_LTROT, output_filename, row.names = FALSE)
+  
   return(merge_LTROT)
   
 }
