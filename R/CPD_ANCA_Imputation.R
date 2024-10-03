6ef082980f8ea2b35f77f8f189c607421c112541
MACTCD68 <COQM@tcd.ie>
2024-10-03 10:05:43 +0100

    Update CPD_ANCA_Imputation.R

--- R/CPD_ANCA_Imputation.R
+++ R/CPD_ANCA_Imputation.R
@@ -32,6 +32,8 @@ CPD_ANCA_Imputation <- function(Encounter, output_dir){
     }
   }
   
+  
+  
   output_filename <- file.path(
     output_dir,
     paste0('Redcap_ANCA_imputation_function_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
