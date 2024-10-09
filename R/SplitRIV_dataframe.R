57262814e8af4e4d178b4bd8139847715361e02f
MACTCD68 <COQM@tcd.ie>
2024-10-09 14:06:30 +0100

    Update SplitRIV_dataframe.R

--- R/SplitRIV_dataframe.R
+++ R/SplitRIV_dataframe.R
@@ -15,13 +15,28 @@
 #' 
 #' @export
 
-SplitRIV_dataframe <- function(RKDdata, algorithm){
+SplitRIV_dataframe <- function(RKDdata, output_dir, algorithm){
   if (is.list(RKDdata) == FALSE) {
     stop("The argument RKDdata need to be a list argument")
+  }
+  
+  stopifnot("Your argument need to be a character"=is.character(output_dir))
+  
+  # Check output directory
+  if (!dir.exists(output_dir)) {
+    stop('Specified output folder does not exist')
   }
   
   algorithm <- match.arg(algorithm, c("Encounter", "Initial" , "IVTherapy" , "Medication" ,"Biopsy" , "Transplant" ,"Complication"))
   
   RKD_data=RKDdata[[which(names(RKDdata)==algorithm)]]
+  
+  output_filename <- file.path(
+    output_dir,
+    paste0('Redcap_Split_frame_',algorithm, "_version", packageVersion('rivpipeline'), "_Date"
+           , Sys.Date(), '.csv')
+  )
+  write.csv(RKD_data, output_filename, row.names = FALSE)
+  
   return(RKD_data)
 }
