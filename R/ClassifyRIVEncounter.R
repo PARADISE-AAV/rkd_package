db09227b357daeae649d71360646bb1916c8b76f
MACTCD68 <COQM@tcd.ie>
2024-10-16 14:36:23 +0100

    Update ClassifyRIVEncounter.R

--- R/ClassifyRIVEncounter.R
+++ R/ClassifyRIVEncounter.R
@@ -76,6 +76,7 @@ ClassifyRIVEncounter = function(RKDdata, output_path, algorithm="All", interval_
   
   if(algorithm ==  "All"){
     Classify_RKD_CPD_Relapse <- CPDRelapse(RKD_data, interval_from_diagnostics)
+    colnames(Classify_RKD_CPD_Relapse)[which(colnames(Classify_RKD_CPD_Relapse)=="Interval.from.diagnosis..months..x")]="Interval.from.diagnosis..months."
     Classify_RKD_Paradise_encounter <- Paradise_Encounter(Classify_RKD_CPD_Relapse, interval_from_diagnostics)
     Classify_RKD_Treatment_OnOff <- CPD_Treatment_OnOff(IV_data, CM_data, Classify_RKD_Paradise_encounter)
     Classify_RKD_Treatment_list <- CPD_Treatment(Classify_RKD_Treatment_OnOff)
