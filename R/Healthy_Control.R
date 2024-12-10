24322f5c22edbe4a63a4794fb63faeaf5878c75b
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2024-12-10 10:36:57 +0000

    Update Healthy_Control.R

--- R/Healthy_Control.R
+++ R/Healthy_Control.R
@@ -9,16 +9,7 @@
 #'
 #' @param RKDdata RIV data from \code{\link{clean_riv}} function
 #' @details The filter of the RIV data for Healthy Control are based on the following filter
-#' *  SVV = GPA
-#' *  AND diagnosis confidence = definite
-#' *  AND SVV (IC) != Anti-GBM, IgA, cryo
-#' *  AND Secondary vasculitis != Yes
-#' *  AND 'Other' != Yes
-#' *  AND 'Medium vessel' != 1 or 2
-#' *  AND 'large vessel' != 1 or 2
-#' *  AND 'variable vessel' != 1 or 2
-#' *  AND (ANCA subtype = PR3 / MPO / both) OR (biopsy = Definite vasculitis AND histologically confirmed = YES)
-#' 
+#' *  Type.of.Patient = Healthy Control
 #' 
 #' @return The Redcap data filter in your folder and an R object
 #' @export
@@ -36,46 +27,12 @@ HealthyControl <- function(RKDdata) {
   }
   
   ###Select on disease Select only GPA and MPA
-  RKD_data_DiseaseFilter <- RKD_data[which(RKD_data$Small.vessel.vasculitis..ANCA.associated. == "Granulomatosis with polyangiitis (Wegener) - Orpha:900"),]
+  RKD_data_DiseaseFilter <- RKD_data[which(RKD_data$Type.of.Patient == "Healthy Control"),]
   
   
-  #####Diagnosis confidence filter
-  RKD_data_DiagnosisFilter <- RKD_data_DiseaseFilter[which(RKD_data_DiseaseFilter$Diagnosis.confidence == "Definite"),]
   
-  ###### Small vessel immune filter
   
-  RKD_data_SmallvesselImmuneFilter <- RKD_data_DiagnosisFilter[which(RKD_data_DiagnosisFilter$Small.vessel.vasculitis..Immune.complex. == ""),]
-  
-  ########Secondary vascularite filter
-  
-  RKD_data_SecondaryFilter <- RKD_data_SmallvesselImmuneFilter[which(RKD_data_SmallvesselImmuneFilter$Secondary.vasculitis != "Yes"),]
-  
-  ########Other filter
-  
-  RKD_data_OtherFilter <- RKD_data_SecondaryFilter
-  
-  #######medium vessel filter
-  
-  RKD_data_MediumVesselFilter <- RKD_data_OtherFilter[which(RKD_data_OtherFilter$Medium.vessel.vasculitis == ""), ]
-  
-  #######large vessel filter
-  
-  RKD_data_largeVesselFilter <- RKD_data_MediumVesselFilter[which(RKD_data_MediumVesselFilter$Large.vessel.vasculitis == ""), ]
-  
-  #####Variable vessel filter
-  
-  RKD_data_VariableVesselFilter <- RKD_data_largeVesselFilter[which(RKD_data_largeVesselFilter$Variable.vessel.vasculitis == ""), ]
-  
-  ####Last filter
-  
-  RKD_data_LastFilter <- RKD_data_VariableVesselFilter[which((RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "PR3" | 
-                                                                RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "MPO" |
-                                                                RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "MPO and PR3") | 
-                                                               
-                                                               RKD_data_VariableVesselFilter$Histologically.confirmed.diagnosis == "Yes"
-  ),]
-  
-  Filter_RKD_data <- RKD_data_LastFilter
+  Filter_RKD_data <- RKD_data_DiseaseFilter
   
   
   
