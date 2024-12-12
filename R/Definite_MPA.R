b27d63499662f73b7f02c9d84bbc2d59708e9e50
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2024-12-12 07:45:02 +0000

    Update Definite_MPA.R

--- R/Definite_MPA.R
+++ R/Definite_MPA.R
@@ -70,9 +70,9 @@ DefiniteMPA <- function(RKDdata) {
   
   RKD_data_LastFilter <- RKD_data_VariableVesselFilter[which((RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "PR3" | 
                                                                 RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "MPO" |
-                                                                RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "MPO and PR3") | 
+                                                                RKD_data_VariableVesselFilter$At.any.point.ANCA.specificity == "MPO and PR3") 
                                                                
-                                                               RKD_data_VariableVesselFilter$Histologically.confirmed.diagnosis == "Yes"
+                                                             | RKD_data_VariableVesselFilter$Histologically.confirmed.diagnosis == "Yes"
   ),]
   
   Filter_RKD_data <- RKD_data_LastFilter
