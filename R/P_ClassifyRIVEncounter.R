9f83d819e507962af0e20b9cade8774d9ee6a114
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2024-12-19 15:51:01 +0000

    Update P_ClassifyRIVEncounter.R

--- R/P_ClassifyRIVEncounter.R
+++ R/P_ClassifyRIVEncounter.R
@@ -80,7 +80,7 @@ ClassifyRIVEncounter = function(RKDdata, output_path, algorithm="All", interval_
     Classify_RKD_Paradise_encounter <- Paradise_Encounter(Classify_RKD_CPD_Relapse, interval_from_diagnostics)
     Classify_RKD_Treatment_OnOff <- CPD_Treatment_OnOff(IV_data, CM_data, Classify_RKD_Paradise_encounter)
     Classify_RKD_Treatment_list <- CPD_Treatment(Classify_RKD_Treatment_OnOff)
-    Classify_RKD_Treatment_list_plus <- CPD_immunosup_med(Classify_RKD_Treatment_list, tempdir())
+    Classify_RKD_Treatment_list_plus <- CPD_immunosup_med(Classify_RKD_Treatment_list)
     Classify_RKD_LTROT_patient <- CPD_LTROT(Classify_RKD_Treatment_list_plus, nb_month)
     Classify_RKD_LTROT_Encounter <- CPD_LTROT_current(Classify_RKD_LTROT_patient, nb_day)
     Classify_RKD_ANCA <- CPD_ANCA(Classify_RKD_LTROT_Encounter)
