dc23b8c8a1ddbdb277fc500296b61c4260b612ea
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-01-08 15:04:37 +0000

    Update CPD_Corticosteroids_on_off.R

--- R/CPD_Corticosteroids_on_off.R
+++ R/CPD_Corticosteroids_on_off.R
@@ -8,23 +8,15 @@
 #' Date: 15-Aug-23
 #'
 #' @param merge_data Data from the merge of encounter and General characteristics in the \code{\link{CPD_Treatment}} function
-#' @param output_dir folder where the Redcap data will be saved
 #' @details
 #' to be added
 #' @import lubridate
 #' @import dplyr
 #' @export
 
-CPD_Corticosteroids_on_off <- function(merge_data, output_dir){
+CPD_Corticosteroids_on_off <- function(merge_data){
   
   stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
-  stopifnot("Your argument need to be a character"=is.character(output_dir))
-  
-  # Check output directory
-  if (!dir.exists(output_dir)) {
-    stop('Specified output folder does not exist')
-  }
-  
   
   merge_data <- merge_data %>%
     dplyr::mutate(Corticosteroids_On_off = dplyr::case_when(
@@ -39,13 +31,6 @@ CPD_Corticosteroids_on_off <- function(merge_data, output_dir){
   c=inner_join(a,b)
   merge_data$Corticosteroids_On_off[ as.numeric(c$x)]="On"
   
-  
-  output_filename <- file.path(
-    output_dir,
-    paste0('Redcap_CPD_Corticosteroids_on_off_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
-           , Sys.Date(), '.csv')
-  )
-  write.csv(merge_data, output_filename, row.names = FALSE)
   return(merge_data)
   
 }
