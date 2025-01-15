97b13fe60215cc21242c703208e1bb5c4d8ed1bd
MACTCD68 <COQM@tcd.ie>
2025-01-15 10:40:03 +0000

    Update CPD_Treatment_OnOff.R

--- R/CPD_Treatment_OnOff.R
+++ R/CPD_Treatment_OnOff.R
@@ -105,36 +105,23 @@ CPD_Treatment_OnOff= function(IV_Therapy, ConMed, merged_data){
     if(is.na(data_merged$Step3[i]) == FALSE){
       data_merged$CPD_treatment[i]="On Treatment"
     }else{
-      if(is.na(data_merged$Step1[i]) == TRUE  & (is.na(data_merged$Step4[i])==TRUE | data_merged$Step4[i] == "On treatment") & (is.na(data_merged$Step5[i])==T | data_merged$Step5[i] == "On treatment")){
-        data_merged$CPD_treatment[i]="On Treatment"
-      }
-      if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == FALSE & (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "Off treatment")){
-        data_merged$CPD_treatment[i]="Manual review"
-      }
-      if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == FALSE &  (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "On treatment") & (is.na(data_merged$Step5[i])==FALSE & data_merged$Step5[i] == "Off treatment")){
-        data_merged$CPD_treatment[i]="Manual review"
-      }
-      if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "On treatment") & (is.na(data_merged$Step7[i])==FALSE)){
-        data_merged$CPD_treatment[i]="Manual review"
-      }
-      if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==TRUE )  & (is.na(data_merged$Step7[i])==FALSE)){
-        data_merged$CPD_treatment[i]="Manual review"
-      }
-      if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==TRUE | data_merged$Step4[i] == "Off treatment") &  data_merged$Step6[i] != "Treatment Status Unknown" & (is.na(data_merged$Step7[i])==FALSE)){
-        data_merged$CPD_treatment[i]="Manual review"
-      }
-      if (is.na(data_merged$Step1[i]) == FALSE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==TRUE | data_merged$Step4[i] == "Off treatment") & (is.na(data_merged$Step5[i])==TRUE | data_merged$Step5[i] == "Off treatment") & data_merged$Step6[i] == "Treatment Status Unknown"){
+      if(is.na(data_merged$Step1[i]) == FALSE   & (is.na(data_merged$Step5[i])==T) & data_merged$Step6[i] == "Treatment Status Unknown" ){
         data_merged$CPD_treatment[i]="Off Treatment"
       }
-      
+      if(is.na(data_merged$Step1[i]) == TRUE  &  (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "On treatment") & data_merged$Step6[i] == "On treatment"){
+        data_merged$CPD_treatment[i]="On Treatment"
+      }
       if (is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==TRUE ) & (is.na(data_merged$Step5[i])==TRUE) & data_merged$Step6[i] == "Treatment Status Unknown"){
         data_merged$CPD_treatment[i]="Treatment Status Unknown"
       }
-      if (is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==TRUE ) & (is.na(data_merged$Step5[i])==TRUE) & data_merged$Step6[i] == "Treatment Status Unknown" & is.na(data_merged$Step7[i])==FALSE){
-        data_merged$CPD_treatment[i]="Off Treatment"
-      }
       if (is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE &  (is.na(data_merged$Step4[i])==TRUE | data_merged$Step4[i] == "Off treatment") & (is.na(data_merged$Step5[i])==TRUE | data_merged$Step5[i] == "Off treatment") & data_merged$Step6[i] == "Prednisolone<=10" & is.na(data_merged$Step7[i]) == TRUE){
         data_merged$CPD_treatment[i]="Prednisolone<=10"
+      }
+      if(is.na(data_merged$Step1[i]) == TRUE   & (is.na(data_merged$Step4[i])==TRUE | data_merged$Step4[i] == "Off treatment") & data_merged$Step6[i] == "Treatment Status Unknown" & is.na(data_merged$Step7[i]) == FALSE){
+        data_merged$CPD_treatment[i]="Off Treatment"
+      }
+      if(is.na(data_merged$Step1[i]) == TRUE & is.na(data_merged$Step2[i]) == TRUE  & (is.na(data_merged$Step4[i])==FALSE & data_merged$Step4[i] == "Off treatment") & (is.na(data_merged$Step5[i])==TRUE ) & data_merged$Step6[i] == "Treatment Status Unknown" ){
+        data_merged$CPD_treatment[i]="Off Treatment"
       }
     }
     
