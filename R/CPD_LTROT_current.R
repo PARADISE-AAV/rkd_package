423b8a5966d66ea4c17dd84eebca5fc83a3c7392
MACTCD68 <COQM@tcd.ie>
2025-01-31 13:01:51 +0000

    Update CPD_LTROT_current.R

--- R/CPD_LTROT_current.R
+++ R/CPD_LTROT_current.R
@@ -34,7 +34,7 @@ CPD_LTROT_current <- function(merge_data, interval=730){
       for(j in 1:m){
         intermax=dat1$interval_from_diagnosis[j]
         dat3 <- dat[which(dat$interval_from_diagnosis<=intermax & dat$interval_from_diagnosis>=intermax-interval),]
-        if(dim(table(dat3$CPD_relapse))==1 & dat3$CPD_relapse[1]=="No Relapse" & dim(table(dat3$CPD_treatment))==1 & dat3$CPD_treatment[1]=="Off Treatment" & dat3$ANCA_Status[nrow(dat3)]=="ANCA Negative"){
+        if(dim(table(dat3$CPD_relapse))==1 & dat3$CPD_relapse[1]=="No Relapse" & dim(table(dat3$CPD_treatment))==1 & dat3$CPD_treatment[1]=="Off Treatment"){
             dat3$LTROT_current[nrow(dat3)]="LTROT"
           }
           
