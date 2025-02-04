376a3f6966eee24a6dd02138a022f346bdba2fbd
MACTCD68 <COQM@tcd.ie>
2025-02-04 16:04:52 +0000

    Update Definite_GPA.R

--- R/Definite_GPA.R
+++ R/Definite_GPA.R
@@ -8,7 +8,7 @@
 #' Date: 24-Jan-23
 #'
 #' @param RKDdata RIV data from \code{\link{clean_riv}} function
-#' @param parameter_name {"name": "parameter_name","desc": "description of the parameter","options": {list of available options},"type": "string or file"}
+#' @param parameter_name {"name": "parameter_name","desc": "description of the parameter","options": (list of available options),"type": "string or file"}
 #' @details The filter of the RIV data for Definite GPA are based on the following filter
 #' *  SVV = GPA
 #' *  AND diagnosis confidence = definite
@@ -24,7 +24,7 @@
 #' @return The Redcap data filter in your folder and an R object
 #' @export
 
-DefiniteGPA <- function(RKDdata) {
+DefiniteGPA <- function(RKDdata, parameter_name) {
   
   ####Test on the argument
   if (is.data.frame(RKDdata) == FALSE) {
