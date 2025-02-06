67582f4655edc32e27b2823ada3c692d2f1a784e
MACTCD68 <COQM@tcd.ie>
2025-02-06 10:51:25 +0000

    Create CPD_vasc_gran.R

---
+++ R/CPD_vasc_gran.R
@@ -0,0 +1,23 @@
+#' @title CPD vascvs gran
+#' @author Matthieu COQ
+#'
+#' @description to be added
+#' 
+#' Version: 1.0
+#' 
+#' Date: 15-Aug-23
+#'
+#' @param merge_data Data from the merge of encounter and General characteristics in the \code{\link{CPD_Treatment}} function
+#' @details
+#' to be added
+#' 
+#' @import lubridate
+#' @import dplyr
+#' @export
+
+CPD_vasc_vs_gran <- function(merge_data){
+  
+  stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
+  
+  
+}
