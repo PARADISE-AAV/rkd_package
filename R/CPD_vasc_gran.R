9ce5961b80905942edee7f1038f1a108b8a873ee
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-02-13 08:45:38 +0000

    Update CPD_vasc_gran.R

--- R/CPD_vasc_gran.R
+++ R/CPD_vasc_gran.R
@@ -19,10 +19,30 @@ CPD_vasc_vs_gran <- function(merge_data){
   
   stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
   
+  merge_data$CPD_vasc_gran="Unknown"
   n=nrow(merge_data)
   for(i in 1:n){
     
     if(merge_data$CPD_relapse[i]=="Definite Relapse"){
+      
+      if(merge_data$ENT...yes..choice.Bloody.nasal.discharge...crusts...ulcers...granulomata.[i] == "Checked" | 
+         merge_data$ENT...yes..choice.Paranasal.sinus.involvement.[i] == "Checked" |
+         merge_data$ENT...yes..choice.Subglottic.stenosis.[i] == "Checked" |
+         merge_data$ENT...yes..choice.Conductive.hearing.loss.[i] == "Checked" |
+         merge_data$ENT...yes..choice.Sensorineural.hearing.loss.[i] == "Checked" |
+         merge_data$Chest...yes..choice.Endobronchial.involvement.[i] == "Checked" |
+         merge_data$Chest...yes..choice.Nodules.or.cavities.[i] == "Checked" |
+         merge_data$Nervous.system...yes..choice.Meningitis.[i] == "Checked" |
+         merge_data$Mucous.membranes...eyes...yes..choice.Significant.proptosis.[i] == "Checked"
+      ){
+        merge_data$CPD_vasc_gran[i] = "Granulomatous"
+      }
+      
+      if(
+        
+      ){
+        merge_data$CPD_vasc_gran[i] = "Vasculitic"
+      }
       
     }
     
