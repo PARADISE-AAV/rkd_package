22d173a8b15b8331ebe4a7b4ff0aa512202a7ef4
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-02-20 15:36:11 +0000

    Update CPD_Treatment_discontinution.R

--- R/CPD_Treatment_discontinution.R
+++ R/CPD_Treatment_discontinution.R
@@ -8,22 +8,16 @@
 #' Date: 15-Aug-23
 #'
 #' @param merge_data Data from the merge of encounter and General characteristics in the \code{\link{CPD_Treatment}} function
-#' @param output_dir folder where the Redcap data will be saved
 #' @details
 #' to be added
 #' @import lubridate
 #' @import dplyr
 #' @export
 
-CPD_treatment_discontunition = function (merge_data, output_dir){
+CPD_treatment_discontunition = function (merge_data){
   
   stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
-  stopifnot("Your argument need to be a character"=is.character(output_dir))
-
-  # Check output directory
-  if (!dir.exists(output_dir)) {
-    stop('Specified output folder does not exist')
-  }
+  
   
   merge_data$Step1_TD = NA
   merge_data$Step2_TD = NA
@@ -31,11 +25,8 @@ CPD_treatment_discontunition = function (merge_data, output_dir){
   merge_data$Step4_TD = NA
   merge_data$Step5_TD = NA
   
-  output_filename <- file.path(
-    output_dir,
-    paste0('Redcap_treatment_discontinuation_function_data_merged', "_version", packageVersion('rivpipeline'), "_Date"
-           , Sys.Date(), '.csv')
-  )
-  write.csv(merge_LTROT, output_filename, row.names = FALSE)
+  
+  
+  
   return(merge_LTROT)
 }
