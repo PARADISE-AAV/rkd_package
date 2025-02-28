f8070b40d134caeb9b98a03bb307e852d1f01b4b
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-02-28 15:10:37 +0000

    Update P_FilterRIV.R

--- R/P_FilterRIV.R
+++ R/P_FilterRIV.R
@@ -9,7 +9,7 @@
 #'
 #' @param RKDdata {"name": "rkd_data","desc": "RIV data from \code{\link{SplitRIV_dataframe}} function","options": (),"type": "file"}
 #' @param output_path {"name": "output_path","desc": "folder where the Redcap data will be saved","options": (),"type": "string"}
-#' @param algorithm {"name": "algorithm","desc": "function use to filter the RKD patient","options": ("Definite GPA", "Definite MPA", "Definite EGPA", "Anti-GBM", "Double positive", "Healthy Control", "IgA),"type": "string"}
+#' @param algorithm {"name": "algorithm","desc": "function use to filter the RKD patient","options": ("Definite GPA", "Definite MPA", "Definite EGPA", "Anti-GBM", "Double positive", "Healthy Control", "IgA", "Cryoglobulinemic"),"type": "string"}
 #' @return The Redcap data with the classification variables in your folder and in an R object 
 #' @details The filter of the RKD data are based on the following filter
 #' * Definite MPA/GPA without secondary vasculitis with more explanation in \code{\link{DemographicFilterRIV}}
@@ -78,6 +78,11 @@ FilterRIV = function(RKDdata, output_path, algorithm) {
   if (is.element("IgA", algorithm)) {
     Filter_RKD_data1 <- IgA(RKD_data)
     Filter_RKD_data1$IgA <- 1
+    Filter_RKD_data <- rbind(Filter_RKD_data, Filter_RKD_data1)
+  }
+  if (is.element("Cryoglobulinemic", algorithm)) {
+    Filter_RKD_data1 <- Cryoglobulinemic(RKD_data)
+    Filter_RKD_data1$Cryoglobulinemic <- 1
     Filter_RKD_data <- rbind(Filter_RKD_data, Filter_RKD_data1)
   }
   
