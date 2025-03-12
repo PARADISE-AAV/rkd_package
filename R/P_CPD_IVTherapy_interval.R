a279e27c9d8f566715ff5b287f969c5b783a7799
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-03-12 10:34:23 +0000

    Update P_CPD_IVTherapy_interval.R

--- R/P_CPD_IVTherapy_interval.R
+++ R/P_CPD_IVTherapy_interval.R
@@ -6,9 +6,9 @@
 #' Version: 1.0
 #' 
 #' Date: 17-Apr-23
+#' @param IVTherapy_data  {"name": "rkd_data","desc": "RIV data from \code{\link{SplitRIV}} function","options": (),"type": "file"}
+#' @param output_dir  {"name": "output_dir","desc": "folder where the Redcap data will be saved","options": (),"type": "string"}
 #'
-#' @param IVTherapy_data list of dataframe from \code{\link{SplitRIV}} function
-#' @param output_dir folder where the Redcap data will be saved
 #' @details
 #' to be added
 #' @import lubridate
