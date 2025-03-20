441d60cfc9602785fa19d3a0415c9bcd337d399d
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-03-20 11:19:29 +0000

    Create P_Harmonisation.R

---
+++ R/P_Harmonisation.R
@@ -0,0 +1,21 @@
+#' @title CPD Harmonisation
+#' @author Matthieu COQ
+#'
+#' @description The Goal is to finalize the harmonisation
+#' 
+#' Version: 1.0
+#' 
+#' Date: 9-May-23
+#'
+#' @param RIVdata  {"name": "rkd_data","desc": "RIV data from \code{\link{ClassifyRIVEncounter}} function","options": (),"type": "file"}
+#' @param output_dir  {"name": "output_dir","desc": "folder where the Redcap data will be saved","options": (),"type": "string"}
+#' @details
+#' to be added
+#' 
+#' @import lubridate
+#' @import DT
+#' @import dplyr
+#' @import fuzzyjoin
+#' @importFrom data.table %like%
+#' @export
+
