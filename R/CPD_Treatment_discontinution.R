bc8b85dac76752efc79578a29a4ad1d8c00822fb
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-03-27 09:53:39 +0000

    Update CPD_Treatment_discontinution.R

--- R/CPD_Treatment_discontinution.R
+++ R/CPD_Treatment_discontinution.R
@@ -17,6 +17,7 @@
 CPD_treatment_discontunition = function (merge_data){
   
   stopifnot("Your argument need to be a data frame"=is.data.frame(merge_data))
+  
   rkd=merge_data
   rkd$Treatment_Switch = "Switch Status Unknown"
   n=nrow(rkd)
@@ -25,14 +26,29 @@ CPD_treatment_discontunition = function (merge_data){
       if(rkd$CPD_treatment[i-1] == "Off Treatment" & rkd$CPD_treatment[i] == "Off Treatment"){
         rkd$Treatment_Switch[i-1] = "Off-Off Switch"
       }
-      if(rkd$ANCA_Status[i-1] == "ANCA Negative" & rkd$ANCA_Status[i] == "ANCA Positive"){
-        rkd$Treatment_Switch[i-1] = "Neg-Pos Switch"
+      if(rkd$CPD_treatment[i-1] == "Off Treatment" & rkd$CPD_treatment[i] == "On Treatment"){
+        rkd$Treatment_Switch[i-1] = "Off-On Switch"
       }
-      if(rkd$ANCA_Status[i-1] == "ANCA Positive" & rkd$ANCA_Status[i] == "ANCA Positive"){
-        rkd$Treatment_Switch[i-1] = "Pos-Pos Switch"
+      if(rkd$CPD_treatment[i-1] == "Off Treatment" & rkd$CPD_treatment[i] == "Prednisolone<=10"){
+        rkd$Treatment_Switch[i-1] = "Off-Pred<=10 Switch"
       }
-      if(rkd$ANCA_Status[i-1] == "ANCA Positive" & rkd$ANCA_Status[i] == "ANCA Negative"){
-        rkd$Treatment_Switch[i-1] = "Pos-Neg Switch"
+      if(rkd$CPD_treatment[i-1] == "On Treatment" & rkd$CPD_treatment[i] == "Off Treatment"){
+        rkd$Treatment_Switch[i-1] = "On-Off Switch"
+      }
+      if(rkd$CPD_treatment[i-1] == "On Treatment" & rkd$CPD_treatment[i] == "Prednisolone<=10"){
+        rkd$Treatment_Switch[i-1] = "On-Pred<=10 Switch"
+      }
+      if(rkd$CPD_treatment[i-1] == "On Treatment" & rkd$CPD_treatment[i] == "On Treatment"){
+        rkd$Treatment_Switch[i-1] = "On-On Switch"
+      }
+      if(rkd$CPD_treatment[i-1] == "Prednisolone<=10" & rkd$CPD_treatment[i] == "Off Treatment"){
+        rkd$Treatment_Switch[i-1] = "Pred<=10-Off Switch"
+      }
+      if(rkd$CPD_treatment[i-1] == "Prednisolone<=10" & rkd$CPD_treatment[i] == "Prednisolone<=10"){
+        rkd$Treatment_Switch[i-1] = "Pred<=10-Pred<=10 Switch"
+      }
+      if(rkd$CPD_treatment[i-1] == "Prednisolone<=10" & rkd$CPD_treatment[i] == "On Treatment"){
+        rkd$Treatment_Switch[i-1] = "Pred<=10-On Switch"
       }
       
     }
