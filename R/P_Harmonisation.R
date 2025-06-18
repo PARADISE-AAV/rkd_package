9dfaa49633ecfed64eff108362d2dd39481c7892
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-06-18 10:21:52 +0100

    Update P_Harmonisation.R

--- R/P_Harmonisation.R
+++ R/P_Harmonisation.R
@@ -34,7 +34,7 @@ CPD_Harmonisation <- function(RIVdata, output_dir){
     stop("You supplied an empty file")
   }
   
-  rkd_data <- RIVdata[ ,c("RKD.ID", "Date.Of.Visit", "interval_from_diagnosis", "CPD_relapse", "rel.method",  "Urinalysis.Protein", "Urinalysis.Blood", "Number.of.major.BVAS.items", "Number.of.minor.BVAS.items", "CPD_vasc_gran", "Nature.of.confirmed.relapse", "CPD_treatment", "Immunosuppressive.medication...other..ATC", "ANCA_Status", "CRP", "Creatinine", "eGFR..calculated." , "Platelet.count.x10.9.L", "Total.white.cell.count.x10.9.L", "Neutrophil.count.x10.9.L", "Lymphocyte.count.x10.9.L", "Neutrophil...Lymphocyte.ratio", "Monocyte.count.x10.9.L", "Absolute.CD19.count..cells.uL.", "ANCA.IF", "Anti.PR3.level", "PR3.titre", "Anti.MPO.level", "MPO.titre", "Immunosuppressive.status", "Corticosteroids", "Current.corticosteroid.dose",  "ANCA_Switch", "anca_kinetics_longterm", "Gender", "Year.of.birth", "Status.x", "Date_Last_Follow_up","Date.of.event", "At.any.point.ANCA.IF.pattern", "At.any.point.ANCA.specificity", "Any.Induction.Treatment", "Any.Maintenance.Treatment", "Required.renal.replacement.therapy.during.first.presentation", "Renal.recovery..independence.from.dialysis..regardless.of.dialysis.duration.", "Date.of.renal.recovery", "renal_recovery_time", "End.stage.kidney.disease", "Date.of.end.stage.kidney.disease..date.of.commencement.on.dialysis.or.transplant..whichever.first.", "ESKD_time", "Date.of.diagnosis", "Age.at.diagnosis", "Small.vessel.vasculitis..ANCA.associated.", "Small.vessel.vasculitis..Immune.complex.", "Diagnosis.confidence")]
+  rkd_data <- RIVdata[ ,c("Paradise.ID", "Date.Of.Visit", "interval_from_diagnosis", "CPD_relapse", "rel.method",  "Urinalysis.Protein", "Urinalysis.Blood", "BVAS.score..calculator.", "Number.of.major.BVAS.items", "Number.of.minor.BVAS.items", "CPD_vasc_gran", "Nature.of.confirmed.relapse", "CPD_treatment", "Immunosuppressive.medication...other..ATC", "ANCA_Status", "CRP", "Creatinine", "eGFR..calculated." , "Platelet.count.x10.9.L", "Total.white.cell.count.x10.9.L", "Neutrophil.count.x10.9.L", "Lymphocyte.count.x10.9.L", "Neutrophil...Lymphocyte.ratio", "Monocyte.count.x10.9.L", "Absolute.CD19.count..cells.uL.", "ANCA.IF", "Anti.PR3.level", "PR3.titre", "Anti.MPO.level", "MPO.titre", "Immunosuppressive.status", "Corticosteroids", "Current.corticosteroid.dose",  "ANCA_Switch", "anca_kinetics_longterm", "Gender", "Year.of.birth", "Status.x", "Date_Last_Follow_up","Date.of.event", "At.any.point.ANCA.IF.pattern", "At.any.point.ANCA.specificity", "Any.Induction.Treatment", "Any.Maintenance.Treatment", "Required.renal.replacement.therapy.during.first.presentation", "Renal.recovery..independence.from.dialysis..regardless.of.dialysis.duration.", "Date.of.renal.recovery", "renal_recovery_time", "End.stage.kidney.disease", "Date.of.end.stage.kidney.disease..date.of.commencement.on.dialysis.or.transplant..whichever.first.", "ESKD_time", "Date.of.diagnosis", "Age.at.diagnosis", "Small.vessel.vasculitis..ANCA.associated.", "Small.vessel.vasculitis..Immune.complex.", "Diagnosis.confidence")]
   
   LTROT <- RIVdata[,grep("LTROT_current", colnames(RIVdata))]
   IS <- RIVdata[,grep("immunosup_med", colnames(RIVdata))]
@@ -44,13 +44,19 @@ CPD_Harmonisation <- function(RIVdata, output_dir){
   
   harmonized_data <- cbind(rkd_data, LTROT, IS, affectedorgan, inductiontreatment, maintenancetreatment)
   
+  colnames(harmonized_data)[2] = "dateOfEncounter"
+  colnames(harmonized_data)[3] = "encounter_interval_from_diagnosis"
+  colnames(harmonized_data)[6] = "urinalysis_protein"
+  colnames(harmonized_data)[7] = "urinalysis_blood"
+  
+  
   output_filename <- file.path(
     output_dir,
     paste0('Redcap_clinical_data_harmonized', "_version", packageVersion('rivpipeline'), "_Date"
            , Sys.Date(), '.csv')
   )
   
-  write.csv(rkd_data, output_filename, row.names = FALSE)
-  return(rkd_data)
+  write.csv(harmonized_data, output_filename, row.names = FALSE)
+  return(harmonized_data)
   
 }
