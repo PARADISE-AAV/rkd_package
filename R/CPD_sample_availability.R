7dc2fb3302d8323a4b93ef6736df918e5b959db7
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-06-24 10:07:05 +0100

    Create CPD_sample_availability.R

---
+++ R/CPD_sample_availability.R
@@ -0,0 +1,48 @@
+#' @title CPD sample availability
+#' @author Matthieu COQ/Yagmur Dogay
+#'
+#' @description The Goal is to finalize the harmonisation
+#' 
+#' Version: 1.0
+#' 
+#' Date: 9-May-23
+#'
+#' @param RIVdata  {"name": "rkd_data","desc": "RIV data from \code{\link{ClassifyRIVEncounter}} function","options": (),"type": "file"}
+#' @param output_dir  {"name": "output_dir","desc": "folder where the Redcap data will be saved","options": (),"type": "string"}
+#' @details
+#' to be added
+#' 
+#' @import lubridate
+#' @import DT
+#' @import dplyr
+#' @import fuzzyjoin
+#' @importFrom data.table %like%
+#' @export
+
+CPD_sample_availability <- function(RIVdata, output_dir){
+  
+  stopifnot("Your argument need to be a data frame"=is.data.frame(RIVdata))
+  stopifnot("Your argument need to be a character"=is.character(output_dir))
+  
+  # Check output directory
+  if (!dir.exists(output_dir)) {
+    stop('Specified output folder does not exist')
+  }
+  
+  # Check that you load a real file
+  if (!ncol(RIVdata) || !nrow(RIVdata)) {
+    stop("You supplied an empty file")
+  }
+  
+  
+  
+  output_filename <- file.path(
+    output_dir,
+    paste0('Redcap_clinical_data_harmonized', "_version", packageVersion('rivpipeline'), "_Date"
+           , Sys.Date(), '.csv')
+  )
+  
+  write.csv(harmonized_data, output_filename, row.names = FALSE)
+  return(harmonized_data)
+  
+}
