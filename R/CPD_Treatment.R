6ed1b93ee9d567b6cb23614e16b4aab6ca4d1794
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-07-25 10:50:17 +0100

    Update CPD_Treatment.R

--- R/CPD_Treatment.R
+++ R/CPD_Treatment.R
@@ -9,7 +9,7 @@
 #'
 #' @param treatment_data Data from IVTherapy from \code{\link{CPD_Treatment_OnOff}} function
 #' @details
-#' this function created a list of treatment per encounter
+#' This function created a list of treatment per encounter
 #' 
 #' @import lubridate
 #' @import DT
