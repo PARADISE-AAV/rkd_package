3674f3ae3a9c9b44d56beb931e076138fe48b6b6
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2025-08-01 15:05:58 +0100

    Update P_Harmonisation.R

--- R/P_Harmonisation.R
+++ R/P_Harmonisation.R
@@ -1,7 +1,7 @@
 #' @title CPD Harmonisation
 #' @author Matthieu COQ
 #'
-#' @description The Goal is to finalize the harmonisation
+#' @description The Goal is to finalize the harmonization and create a file with only the cor variable
 #' 
 #' Version: 1.0
 #' 
