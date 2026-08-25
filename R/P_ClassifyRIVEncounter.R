d8584e4612bb1c516c144d2f24ca7c576019ec74
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2026-08-25 09:09:18 +0100

    Update P_ClassifyRIVEncounter.R

--- R/P_ClassifyRIVEncounter.R
+++ R/P_ClassifyRIVEncounter.R
@@ -109,8 +109,7 @@ ClassifyRIVEncounter = function(RKDdata, output_path, algorithm="All", interval_
     Classify_RKD_Vasc_Gran <- CPD_vasc_vs_gran(Classify_RKD_LTROT_Encounter)
     Classify_RKD_TreatmentSwitch <- CPD_treatment_discontunition(Classify_RKD_Vasc_Gran)
     Classify_RKD_data <- CPD_Kidney_function(Classify_RKD_TreatmentSwitch, Renal)
-    #Classify_RKD_data_ISI_base <-compute_cumulative_isi_score_base(Classify_RKD_data, IVTherapy_data, ConMed_data)
-    Classify_RKD_data_ISI_base<-Classify_RKD_data
+    Classify_RKD_data_ISI_base <-compute_cumulative_isi_score_base(Classify_RKD_data, IVTherapy_data, ConMed_data)
   }
 
   files_test <-  list.dirs(output_path)
