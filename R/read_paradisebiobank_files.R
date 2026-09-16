faf2ded4c4c33e1efc43d06099c08d98a274fa17
MACTCD68 <120493801+MACTCD68@users.noreply.github.com>
2026-09-16 09:39:49 +0100

    Create read_paradisebiobank_files.R

---
+++ R/read_paradisebiobank_files.R
@@ -0,0 +1,31 @@
+#' Read multiple Paradise/Biobank files and store as variables
+#'
+#' @description
+#' This function reads multiple Paradise_Biobank CSV files from the specified paths
+#' and assigns them to variables in the global environment for later use.
+#'
+#' @param files A named list of file paths. The **names** of the list will be used
+#'   as the variable names in the global environment. Example:
+#'   \code{list(Urine = "path/to/urine.csv", Blood = "path/to/blood.csv")}
+#'
+#' @return None. The data frames are stored in the global environment.
+#'
+#' @export
+#' 
+#' 
+
+read_paradisebiobank_files <- function(files) {
+  for (var_name in names(files)) {
+    file_path <- files[[var_name]]
+    
+    # Read CSV file
+    df <- read.csv(file_path)
+    
+    # Assign to global environment with the given name
+    assign(var_name, df, envir = .GlobalEnv)
+
+  }
+}
+
+
+
